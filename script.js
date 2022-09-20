12e92340f67fee8cb64358c2540247f8b176245c
amarjeetk123 <ak123del@gmail.com>
2022-09-20 17:08:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let str = "";
+document.addEventListener("input", fn_name);
+function fn_name(){
+    let user_input = document.getElementById("user_input").value
+    str = user_input  
+}
+  
+
+
+function check(){
+  let str_reverse = ""
+    for(let i=str.length-1;i>=0;i--){
+        str_reverse =  str_reverse + str[i]
+     } 
+     if(str == str_reverse){
+        document.getElementById("res").innerHTML= "Text is Palindrom"
+     }
+    else{
+        document.getElementById("res").innerHTML="Not Pakindrom"
+    }
+}
+
+function reset(){
+    document.getElementById("user_input").value = "" 
+    document.getElementById("res").innerHTML= ""
+}
+
